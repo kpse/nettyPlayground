86d6b2558ac1d44f6279f5c3c9f9d6ebf7466223
Suo Qin <kpsesuo@gmail.com>
2014-11-20 23:06:25 +0800

    add logback lib

--- src/main/scala/com.kulebao/SimpleServerHandler.scala
+++ src/main/scala/com.kulebao/SimpleServerHandler.scala
@@ -7,14 +7,19 @@ import java.util.Date
 
 import io.netty.buffer.ByteBuf
 import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
+import org.slf4j.LoggerFactory
 
 class SimpleServerHandler extends ChannelInboundHandlerAdapter {
 
   override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef) = {
+    val logger = LoggerFactory.getLogger("SimpleServerHandler")
     val buf = ctx.alloc().buffer()
     val local: ByteBuf = msg.asInstanceOf[ByteBuf]
     val currentTimeMillis = System.currentTimeMillis()
-    val output: String = String.format("You just input: %s at %s \n", local.toString(Charset.defaultCharset()), new Date(currentTimeMillis).toString)
+    val input: String = local.toString(Charset.defaultCharset())
+    logger.debug(input)
+    val output: String = String.format("You just input: %s at %s \n", input, new Date(currentTimeMillis).toString)
+    logger.debug(output)
     val byteBuffer = copiedBuffer(output, Charset.defaultCharset())
     buf.writeBytes(byteBuffer)
     local.release()
